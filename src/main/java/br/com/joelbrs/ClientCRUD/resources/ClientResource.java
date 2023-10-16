61ff5404557ea4b2f70b2ebc89d6a6bd257cf3a3
Joel Filho <joelfilho02812@gmail.com>
2023-10-16 12:33:48 -0300

    feat: add client by id

--- src/main/java/br/com/joelbrs/ClientCRUD/resources/ClientResource.java
+++ src/main/java/br/com/joelbrs/ClientCRUD/resources/ClientResource.java
@@ -1,11 +1,13 @@
 package br.com.joelbrs.ClientCRUD.resources;
 
 import br.com.joelbrs.ClientCRUD.dtos.ClientDTO;
+import br.com.joelbrs.ClientCRUD.models.Client;
 import br.com.joelbrs.ClientCRUD.services.ClientService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,12 @@ public class ClientResource {
         Page<ClientDTO> dto = service.findAll(pageable);
 
         return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO client = service.findById(id);
+
+        return ResponseEntity.ok(client);
     }
 }
--- src/main/java/br/com/joelbrs/ClientCRUD/services/ClientService.java
+++ src/main/java/br/com/joelbrs/ClientCRUD/services/ClientService.java
@@ -3,10 +3,13 @@ package br.com.joelbrs.ClientCRUD.services;
 import br.com.joelbrs.ClientCRUD.dtos.ClientDTO;
 import br.com.joelbrs.ClientCRUD.models.Client;
 import br.com.joelbrs.ClientCRUD.repositories.ClientRepository;
+import br.com.joelbrs.ClientCRUD.services.exceptions.ResourceNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -22,5 +25,12 @@ public class ClientService {
         Page<Client> clients =  repository.findAll(pageable);
 
         return clients.map(ClientDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Resource Not Found, ID: " + id));
+
+        return new ClientDTO(client);
     }
 }
