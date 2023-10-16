232f094b3eec242fa42e5c65119da75a95904ca6
Joel Filho <joelfilho02812@gmail.com>
2023-10-16 15:49:06 -0300

    fix: seedings clients

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('725.496.831-37', 'Ester Adriana Luna da Silva', 1900.00, null, 3);
-INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('561.809.425-87', 'Benjamin Enzo Corte Real', 87832.42, null, 0);
-INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('721.856.776-24', 'Guilherme Kevin Emanuel da Rosa', 1230.98, null, 4);
-INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('437.049.387-00', 'Luciana Laura Nicole Campos', 4302.21, null, 1);
-INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('005.263.576-73', 'Matheus Martin Nascimento', 2429.00, null, 6);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('72549683137', 'Ester Adriana Luna da Silva', 1900.00, '2023-10-16', 3);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('56180942587', 'Benjamin Enzo Corte Real', 87832.42, '2023-10-16', 0);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('72185677624', 'Guilherme Kevin Emanuel da Rosa', 1230.98, '2023-10-16', 4);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('43704938700', 'Luciana Laura Nicole Campos', 4302.21, '2023-10-16', 1);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('00526357673', 'Matheus Martin Nascimento', 2429.00, '2023-10-16', 6);
