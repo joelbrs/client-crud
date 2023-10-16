424bb7064813070bcff879c0a0407f6036f5dc41
Joel Filho <joelfilho02812@gmail.com>
2023-10-16 12:16:29 -0300

    feat: add seeding of 5 clients

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('725.496.831-37', 'Ester Adriana Luna da Silva', 1900.00, null, 3);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('561.809.425-87', 'Benjamin Enzo Corte Real', 87832.42, null, 0);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('721.856.776-24', 'Guilherme Kevin Emanuel da Rosa', 1230.98, null, 4);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('437.049.387-00', 'Luciana Laura Nicole Campos', 4302.21, null, 1);
+INSERT INTO tb_client (cpf, name, income, birth_date, children) VALUES ('005.263.576-73', 'Matheus Martin Nascimento', 2429.00, null, 6);
